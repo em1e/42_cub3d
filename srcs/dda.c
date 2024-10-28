345667dedb51f3e3ee9742d5b040b6ad468e781b
Jaakko Junttila <jajuntti@student.hive.fi>
2024-10-28 15:42:07 +0200

    Reverts to using old fisheye adjustment

--- srcs/dda.c
+++ srcs/dda.c
@@ -6,7 +6,7 @@
 /*   By: jajuntti <jajuntti@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 09:49:02 by vkettune          #+#    #+#             */
-/*   Updated: 2024/10/28 13:36:54 by jajuntti         ###   ########.fr       */
+/*   Updated: 2024/10/28 15:36:45 by jajuntti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,7 +91,7 @@ void	calculate_values(t_cub3d *kissa, t_ray *ray)
 		ray->perp_dist = ray->ray_len->y - ray->step_len->y;
 	else
 		ray->perp_dist = ray->ray_len->x - ray->step_len->x;
-	ray->scaled_height = floor(kissa->wall_height / ray->perp_dist / ray->fishey_adjust);//(ray->line_len * ray->fishey_adjust));
+	ray->scaled_height = floor(kissa->wall_height / ray->line_len / ray->fishey_adjust); //ray->perp_dist / ray->fishey_adjust);
 	if (ray->scaled_height < MLX_HEIGHT)
 	{
 		ray->offset = 0;
